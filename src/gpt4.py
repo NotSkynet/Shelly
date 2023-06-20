4eddd14157b03a9c84424bd25b54d4dad0be9760
NotSkynet <caeden@caedenlange.com>
2023-06-19 20:10:42 -0700

    changes to gpt4.py

--- src/gpt4.py
+++ src/gpt4.py
@@ -2,67 +2,50 @@ import os
 import openai
 import json
 
-# Your OpenAI API key
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+# Get the OpenAI API key from the environment variables
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# The GPT-4 model to use
-MODEL = "gpt-4-0613"
+if openai.api_key is None:
+    raise ValueError("Missing OpenAI API key. Please set the 'OPENAI_API_KEY' environment variable.")
 
-# Set the OpenAI API key 
-openai.api_key = OPENAI_API_KEY
-
-def send_input(input_text):
-    """Sends input text to GPT-4 and returns the response"""
-
-    currpath = os.path.expanduser('~')
-
-    available_functions = {
-        "terminal_command_executor": terminal_command_executor,
-    }
-
-    functions = [
-        {
-            "name": "terminal_command_executor",
-            "description": "Runs all the instructions specified by the user in one single MacOS terminal command, use this whenever the user asks for file creation or manipulation as well",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "command": {"type": "string", "description": "The terminal command to execute, e.g. If the user wants to list all files in the current folder, ls."},
-                },
-                "required": ["command"],
+# Define the available function for GPT-4
+functions = [
+    {
+        "name": "terminal_command_executor",
+        "description": "Runs all the instructions specified by the user in one single terminal command",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "command": {"type": "string", "description": "The terminal command to execute."},
             },
+            "required": ["command"],
         },
-    ]
-    # Create the GPT-4 response
+    }
+]
+
+def send_input(user_input, context):
+    # Add the user input to the messages
+    messages = context + [{"role": "user", "content": user_input}]
+
+    # Send the messages to GPT-4
     response = openai.ChatCompletion.create(
-            model="gpt-4-0613",
-            messages=messages,
-            function_call="auto",
-            functions=functions
-        )
-
-    # Parse the GPT-4 response into a JSON object
-    response_json = json.loads(response["choices"][0]["text"])
-
-    # Return the JSON response
-    return response_json
-
-def send_output(output_text):
-    """Sends command output text to GPT-4 and returns the next command"""
-
-    # Create the GPT-4 response
-    response = openai.ChatCompletion.create(
-        model=MODEL,
-        prompt=output_text,
-        temperature=0.7,
-        max_tokens=100,
-        top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.5
+        model="gpt-4-0613",
+        messages=messages,
+        function_call="auto",
+        functions=functions
     )
 
-    # Parse the GPT-4 response into a JSON object
-    response_json = json.loads(response["choices"][0]["text"])
+    # Get the response message from GPT-4
+    response_message = response["choices"][0]["message"]
 
-    # Return the JSON response
-    return response_json
+    # If the response message contains a function call, get the function name and arguments
+    if response_message.get("function_call"):
+        function_name = response_message["function_call"]["name"]
+        function_args = json.loads(response_message["function_call"]["arguments"])
+
+        # If the function name is "terminal_command_executor", return the command
+        if function_name == "terminal_command_executor":
+            return function_args.get("command")
+
+    # If the response message does not contain a function call, return the content of the message
+    return response_message["content"]
